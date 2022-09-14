b6f161d65aa1956ee249b04ebbcf60ec36caf59f
simonmaguire <simonmaguire@live.com>
2022-09-14 14:39:34 -0700

    basic form ui done

--- src/pottery-web-app/src/components/GlazingSection.tsx
+++ src/pottery-web-app/src/components/GlazingSection.tsx
@@ -1,22 +1,26 @@
-import { Form, Card } from "react-bootstrap";
+import { Form, Container, Card } from "react-bootstrap";
 
 function GlazingSection() {
   return (
     <Card className="form-section">
-      <Card.Header className="section-header">Glazing</Card.Header>
+      <Card.Header as="h2" className="section-header">
+        Glazing
+      </Card.Header>
       <Card.Body>
-        <Form.Group>
-          <Form.Label>glazes</Form.Label>
-          <Form.Control type="text" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Glaze Notes</Form.Label>
-          <Form.Control as="textarea" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Glaze Pictures</Form.Label>
-          <Form.Control type="image" />
-        </Form.Group>
+        <Container>
+          <Form.Group>
+            <Form.Label>Glazes</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Glaze Notes</Form.Label>
+            <Form.Control as="textarea" />
+          </Form.Group>
+          {/* <Form.Group>
+            <Form.Label>Glaze Pictures</Form.Label>
+            <Form.Control type="image" />
+          </Form.Group> */}
+        </Container>
       </Card.Body>
     </Card>
   );
